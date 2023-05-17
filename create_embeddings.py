3fbf77fc52e80e78f79657c4956aed43dac179cd
Lim Chee Kin <limcheekin@vobject.com>
2023-05-17 10:33:13 +0800

    feat: created vector stores for each embedding models for evaluation

---
+++ create_embeddings.py
@@ -0,0 +1,39 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredHTMLLoader
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+import pickle
+import os
+
+PICKLE_FILE = os.environ['PICKLE_FILE']
+EMBEDDINGS_MODEL_NAME = os.environ['EMBEDDINGS_MODEL_NAME']
+
+def ingest_data():
+    file_paths = None
+    with open('html_files_index.txt', 'r') as file:
+        file_paths = file.readlines()
+
+    docs = []
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=450, chunk_overlap=20)
+
+    print("Load HTML files locally...")
+    for i, file_path in enumerate(file_paths):
+        file_path = file_path.rstrip("\n")
+        doc = UnstructuredHTMLLoader(file_path).load()
+        splits = text_splitter.split_documents(doc)
+        docs.extend(splits)
+        print(f"{i+1})Split {file_path} into {len(splits)} chunks")
+
+    print("Load data to FAISS store")
+    embeddings = HuggingFaceEmbeddings(
+        model_name=EMBEDDINGS_MODEL_NAME)
+    store = FAISS.from_documents(docs, embeddings)
+
+    print(f"Save to {PICKLE_FILE}")
+    with open(PICKLE_FILE, "wb") as f:
+        pickle.dump(store, f)
+
+
+if __name__ == "__main__":
+    ingest_data()
---
+++ create_embeddings.sh
@@ -0,0 +1,3 @@
+PICKLE_FILE=all_datasets_v3_mpnet-base.pkl EMBEDDINGS_MODEL_NAME=flax-sentence-embeddings/all_datasets_v3_mpnet-base python create_embeddings.py > datasets_v3_mpnet-base.log
+PICKLE_FILE=all-MiniLM-L6-v2.pkl EMBEDDINGS_MODEL_NAME=sentence-transformers/all-MiniLM-L6-v2 python create_embeddings.py > MiniLM-L6-v2.log
+PICKLE_FILE=all-mpnet-base-v2.pkl EMBEDDINGS_MODEL_NAME=sentence-transformers/all-mpnet-base-v2 python create_embeddings.py > mpnet-base-v2.log
