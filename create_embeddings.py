3b1bc3c521f4497b8545b61306fc476b504590d5
Lim Chee Kin <limcheekin@vobject.com>
2023-06-17 12:21:10 +0800

    feat: updated create embeddings to use Ct2BertEmbeddings

--- create_embeddings.py
+++ create_embeddings.py
@@ -2,11 +2,12 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredHTMLLoader
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
-import pickle
+from custom.embeddings.ctranslate2 import Ct2BertEmbeddings
 import os
 
 PICKLE_FILE = os.environ['PICKLE_FILE']
 EMBEDDINGS_MODEL_NAME = os.environ['EMBEDDINGS_MODEL_NAME']
+
 
 def ingest_data():
     file_paths = None
@@ -26,13 +27,23 @@ def ingest_data():
         print(f"{i+1})Split {file_path} into {len(splits)} chunks")
 
     print("Load data to FAISS store")
-    embeddings = HuggingFaceEmbeddings(
-        model_name=EMBEDDINGS_MODEL_NAME)
+    # embeddings = HuggingFaceEmbeddings(
+    #    model_name=EMBEDDINGS_MODEL_NAME)
+    model_name = ""
+    model_kwargs = {'device': 'cpu', 'compute_type': "int8"}
+    encode_kwargs = {'batch_size': 32,
+                     'convert_to_numpy': True, 'normalize_embeddings': True}
+    embeddings = Ct2BertEmbeddings(
+        model_name=EMBEDDINGS_MODEL_NAME,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
     store = FAISS.from_documents(docs, embeddings)
 
     print(f"Save to {PICKLE_FILE}")
-    with open(PICKLE_FILE, "wb") as f:
-        pickle.dump(store, f)
+    store.save_local(PICKLE_FILE)
+    # with open(PICKLE_FILE, "wb") as f:
+    #    pickle.dump(store, f)
 
 
 if __name__ == "__main__":
--- create_embeddings.sh
+++ create_embeddings.sh
@@ -1,3 +1,5 @@
-PICKLE_FILE=all_datasets_v3_mpnet-base.pkl EMBEDDINGS_MODEL_NAME=flax-sentence-embeddings/all_datasets_v3_mpnet-base python create_embeddings.py > datasets_v3_mpnet-base.log
-PICKLE_FILE=all-MiniLM-L6-v2.pkl EMBEDDINGS_MODEL_NAME=sentence-transformers/all-MiniLM-L6-v2 python create_embeddings.py > MiniLM-L6-v2.log
-PICKLE_FILE=all-mpnet-base-v2.pkl EMBEDDINGS_MODEL_NAME=sentence-transformers/all-mpnet-base-v2 python create_embeddings.py > mpnet-base-v2.log
+#PICKLE_FILE=all_datasets_v3_mpnet-base.pkl EMBEDDINGS_MODEL_NAME=flax-sentence-embeddings/all_datasets_v3_mpnet-base python create_embeddings.py > datasets_v3_mpnet-base.log
+#PICKLE_FILE=all-MiniLM-L6-v2.pkl EMBEDDINGS_MODEL_NAME=sentence-transformers/all-MiniLM-L6-v2 python create_embeddings.py > MiniLM-L6-v2.log
+#PICKLE_FILE=all-mpnet-base-v2.pkl EMBEDDINGS_MODEL_NAME=sentence-transformers/all-mpnet-base-v2 python create_embeddings.py > mpnet-base-v2.log
+#PICKLE_FILE=e5-large-v2.pkl EMBEDDINGS_MODEL_NAME=intfloat/e5-large-v2 python create_embeddings.py > e5-large-v2.log
+PICKLE_FILE=e5-large-v2-ct2 EMBEDDINGS_MODEL_NAME=../ct2/ct2fast-e5-large-v2 python create_embeddings.py > e5-large-v2-ct2.log
