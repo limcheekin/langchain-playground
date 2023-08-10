a78bcde701bbf2f3aaba34bcb5ec846993ff6ab9
Lim Chee Kin <limcheekin@vobject.com>
2023-08-10 10:12:58 +0800

    feat: updated create embeddings and evaluation notebook for e5-large-v2 vs bge-large-en

--- create_embeddings.py
+++ create_embeddings.py
@@ -2,6 +2,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredHTMLLoader
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 from custom.embeddings.ctranslate2 import Ct2BertEmbeddings
 from open.text.embeddings.openai import OpenAIEmbeddings
 from open.text.embeddings.huggingface import E5Embeddings
@@ -10,6 +11,12 @@ import pickle
 
 PICKLE_FILE = os.environ['PICKLE_FILE']
 EMBEDDINGS_MODEL_NAME = os.environ['EMBEDDINGS_MODEL_NAME']
+E5_EMBED_INSTRUCTION = "passage: "
+E5_QUERY_INSTRUCTION = "query: "
+BGE_EN_EMBED_INSTRUCTION = ""
+BGE_EN_QUERY_INSTRUCTION = "Represent this sentence for searching relevant passages: "
+BGE_ZH_EMBED_INSTRUCTION = ""
+BGE_ZH_QUERY_INSTRUCTION = "为这个句子生成表示以用于检索相关文章："
 
 
 def ingest_data():
@@ -33,14 +40,28 @@ def ingest_data():
         print(f"{i+1})Split {file_path} into {len(splits)} chunks")
 
     print("Load data to FAISS store")
+    encode_kwargs = {"normalize_embeddings": True}
     if EMBEDDINGS_MODEL_NAME.startswith("universal-sentence-encoder"):
         print("Use universal-sentence-encoder model")
         embeddings = OpenAIEmbeddings(
             openai_api_base="http://localhost:8000/v1")
     elif "e5" in EMBEDDINGS_MODEL_NAME:
-        print("Use E5Embeddings class")
-        encode_kwargs = {"normalize_embeddings": True}
-        embeddings = E5Embeddings(encode_kwargs=encode_kwargs)
+        print("Use E5Embeddings")
+        embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDINGS_MODEL_NAME,
+                                                   embed_instruction=E5_EMBED_INSTRUCTION,
+                                                   query_instruction=E5_QUERY_INSTRUCTION,
+                                                   encode_kwargs=encode_kwargs)
+    elif EMBEDDINGS_MODEL_NAME.startswith("BAAI/bge-") and EMBEDDINGS_MODEL_NAME.endswith("-en"):
+        embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDINGS_MODEL_NAME,
+                                                   embed_instruction=BGE_EN_EMBED_INSTRUCTION,
+                                                   query_instruction=BGE_EN_QUERY_INSTRUCTION,
+                                                   encode_kwargs=encode_kwargs)
+    elif EMBEDDINGS_MODEL_NAME.startswith("BAAI/bge-") and EMBEDDINGS_MODEL_NAME.endswith("-zh"):
+        embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDINGS_MODEL_NAME,
+                                                   embed_instruction=BGE_ZH_EMBED_INSTRUCTION,
+                                                   query_instruction=BGE_ZH_QUERY_INSTRUCTION,
+                                                   encode_kwargs=encode_kwargs)
+
     else:
         embeddings = HuggingFaceEmbeddings(
             model_name=EMBEDDINGS_MODEL_NAME)
--- create_embeddings.sh
+++ create_embeddings.sh
@@ -4,3 +4,4 @@
 PICKLE_FILE=e5-large-v2.pkl EMBEDDINGS_MODEL_NAME=intfloat/e5-large-v2 python create_embeddings.py > e5-large-v2.log
 #PICKLE_FILE=e5-large-v2-ct2 EMBEDDINGS_MODEL_NAME=../ct2/ct2fast-e5-large-v2 python create_embeddings.py > e5-large-v2-ct2.log
 #PICKLE_FILE=universal-sentence-encoder-large-v5.pkl EMBEDDINGS_MODEL_NAME=universal-sentence-encoder-large/5 python create_embeddings.py > universal-sentence-encoder-large-v5.log
+#PICKLE_FILE=bge-large-en.pkl EMBEDDINGS_MODEL_NAME=BAAI/bge-large-en python create_embeddings.py > bge-large-en.log
